bfc4c81e373c40a5fe8ff231738fc47a7b642aa0
saulkurosaki <saulkurosaki13@gmail.com>
2025-04-02 14:25:41 -0600

    feat(AuthLayout): implement the rotating files image along the logo svg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="flex min-h-screen">
       <section className="bg-brand p-10">
         <div>
-          <Image src='/favicon.ico' alt="logo" width={16} height={16} className="h-auto" />
+          <Image src='/assets/icons/logo-full.svg' alt="logo" width={224} height={82} className="h-auto" />
 
           <div className="space-y-5 text-white">
             <h1 className="h1">
@@ -15,6 +15,8 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
               This is a place where you can store all your documents
             </p>
           </div>
+
+          <Image src='/assets/images/files.png' alt="Files" width={342} height={342} className="transition-all hover:rotate-2 hover:scale-105" />
         </div>
       </section>
       {children}
